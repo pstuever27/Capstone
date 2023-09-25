6cf35e822171f57154d735002cdd808ef49a998a
Paul Stuever <pstuever27@gmail.com>
2023-09-24 20:35:55 -0500

    whoops

--- Server Testing Stuff/main.js
+++ Server Testing Stuff/main.js
@@ -1,22 +1,3 @@
-// const mysql = require('mysql');
-// const connection = new mysql.createConnection({
-//     host: 'localhost',
-//     user: 'pstuever27',
-//     password: 'AVaPooTy1053',
-//     database: 'TABLES'
-// })
-
-// connection.connect(function (err) {
-//     if (err) throw err;
-//     console.log("Connected!");
-//     var sql = "INSERT INTO room (roomCode) VALUES ?";
-//     var value = "ABCD";
-//     connection.query(sql, value, function (err, result) {
-//         if (err) throw err;
-//         console.log("Inserted!");
-//     });
-// });
-
 const globals = {
     errorTimeout: -1,
     username: null,
