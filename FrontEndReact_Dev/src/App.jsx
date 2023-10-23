b6827f706fc756857fc9057d59854773c551ca9b
MistrrrMedia <music.mistrrr@gmail.com>
2023-10-22 21:30:43 -0500

    Changed Styling, Added Proxima Nova

--- FrontEndReact_Dev/src/App.jsx
+++ FrontEndReact_Dev/src/App.jsx
@@ -27,6 +27,9 @@ import TextField from '@mui/material/TextField'; // imports textfield component 
 import Autocomplete from '@mui/material/Autocomplete'; // imports autocomplete component of material UI for dynamically rendering search results of search bar
 import { useAPI, getAuthUrl } from './SpotifyAPI'; // imports useAPI function from SpotifyAPI.js for making spotify api calls
 
+// for use when importing proxima nova
+// import Proxima_Nova from 'https://use.typekit.net/wwk0mzk.css';
+
 function App() { // app function to wrap all the contents of the webpage
   const [searchResults, setSearchRes] = useState([]); // state to save search results to be rendered in search bar
   //spotify api call initizations 
@@ -75,19 +78,46 @@ function App() { // app function to wrap all the contents of the webpage
   const [inputVal, setInputValue] = useState(""); // uses a save state to set the text that the user is typing into the search bar to a variable for passing into the api calls and other uses throughout the program
   const [songQueue, { enqueue, dequeue, peek }] = useQueueState([]); // uses the queuestate react save state to maintain the state of the queue over render cycles, and includes the enqueue, dequeue, and peek queue methods for the songQueue alias
 
+  // this will allow us to override the typography in mui
+  // const font_override = createTheme({
+  //   typography: {
+  //     fontFamily: 'proxima-nova, Arial, Helvetica, sans-serif',
+  //   },
+  //   components: {
+  //     MuiCssBaseLine: {
+  //       styleOverrides: 
+  //       `
+  //         @font-face {
+  //           font-family: 'proxima-nova';
+  //           font-style: normal;
+  //           font-display: swap;
+  //           font-weight: 400;
+  //           src: url( ${ Proxima_Nova } ), format( 'woff2' )
+  //           unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+  //         }
+  //       `,
+  //     },
+  //   },
+  // });
+
   return ( // this is what is returned to the webpage to be rendered
     <> {/* parent element to wrap all divs together in return statement */}
+      {/* <ThemeProvider theme={font_override}>
+        <CssBaseline /> 
+      </ThemeProvider> */}
+      {/* in use for overriding mui font */}
+
       <div> {/* div for site logo */}
         <a href="https://songsync.live" target="_blank" rel="noreferrer"> {/* when clicking on the Songsync logo, it will redirect to our website */}
           <img src="./src/assets/logo.png" className="logo" alt="Songsync logo" /> {/* renders our songsync logo to the webpage */}
         </a>
       </div>
       <div className="searchDiv"> {/* div for search bar, using the searchDiv styling */}
-      <h1 style={{textShadow:'1px 1px 2px black'}}>Search Bar Prototype</h1> {/* renders search bar title with text shadow */}
+      <h1 style={{left:500, float:'left', width: 'max-width'}}>Search</h1> {/* renders search bar title with text shadow */}
         <Autocomplete  // autocomplete MUI component for rendering the search results under the search bar as the user types
           disablePortal  // allows for customizing the style of the popper component, which isn't possible for portal-based autocomplete components
           autoHighlight // automatically does a focused highlight on the first search result, so the user can quickly type and hit the enter key on that top result, or use the arrow keys to select a different one from the list
-          noOptionsText={''} // this makes it so that when there are no search options for the inputted text, it will just be a blank empty space below, rather than showing "No options" or other text there. This looks more streamlined.
+          noOptionsText={'Oops! Nothing :('} // this makes it so that when there are no search options for the inputted text, it will just be a blank empty space below, rather than showing "No options" or other text there. This looks more streamlined.
           id="search-box" //sets the html id of this autocomplete tag to be search-box
           value={songChoice} // sets the song that the user selected from the search results list to the songChoice save state
           inputValue={inputVal} // saves the text inputted to the search bar by the user to the inputVal save state
@@ -101,8 +131,8 @@ function App() { // app function to wrap all the contents of the webpage
           isOptionEqualToValue={(option,value)=>`${option.name} - ${option.artists[0].name}`===`${value.name} - ${value.artists[0].name}`} //used to eliminate a warning
           getOptionLabel={searchResult => `${searchResult.name} - ${searchResult.artists[0].name}`} //renders the song option text in the dropdown in the format "Track - Artist"
           options={searchResults} // the "options" prop takes an array of what will be shown as the listed options that the user can search through. These are set to the searchResults array filled with spotify search results from the search() function
-          sx={{ width: 300,}} // sets the width of the search box to be 300 px
-          renderInput={(params) => <TextField {...params} label="Search Songs" />} // this ties it all together, rendering the textfield and search results based off the inputted params. the label "Search Songs" is the ghost placeholder text that renders in the text field when empty.
+          sx={{ width: 'max-width', fontFamily: 'proxima-nova' }} // sets the width of the search box to be 300 px, font family to proxima-nova
+          renderInput={(params) => <TextField {...params} label="Search Songs" sx={{ fontFamily: 'proxima-nova' }}/>} // this ties it all together, rendering the textfield and search results based off the inputted params. the label "Search Songs" is the ghost placeholder text that renders in the text field when empty.
         />
         
         <button onClick={() => addToQueue()} style={{left:500, float:'right',}}>Add to Queue</button>{/* clicking button calls the function to add song to queue */}
@@ -111,7 +141,7 @@ function App() { // app function to wrap all the contents of the webpage
 
       </div>
       <div className='qDiv'> {/* queue div with qDiv css styling */}
-      <h1 style={{ margin: '20px', textShadow:'1px 1px 2px black' }}>Queue</h1> {/* renders queue title with text shadow */}
+      <h1 style={{ margin: '20px' }}>Queue</h1> {/* renders queue title with text shadow */}
         <div style={{ //styling of the queue render
             display: 'flex',  // using flex display paradigm
             flexDirection: 'horizontal', //rendering horizontally
@@ -119,8 +149,10 @@ function App() { // app function to wrap all the contents of the webpage
             height: '60px', //60 px tall
             fontSize: '20px', //font size is 20px
             margin: '20px',  //margins around queue render are 20px
-            borderTop: '2px solid green', //the top and bottom borders are 2px of green
-            borderBottom: '2px solid green'
+            // borderTop: '2px solid green', //the top and bottom borders are 2px of green
+            // borderBottom: '2px solid green'
+            background: 'rgba( 0, 0, 0, 0.2 )',
+            borderRadius: '5px'
         }}>
             {songQueue.map((item) => { //maps each song of the queue to a div rendering of the song name
               return ( //beings return statement that for arrow function
@@ -132,7 +164,7 @@ function App() { // app function to wrap all the contents of the webpage
                     background: "#1189bd", //specifies the color of the background of the song bubble
                     borderRadius: "5px", //specifies how rounded the corners of the song bubble are
                     margin: "10px", //distance between song bubbles within the queue render is 10 px
-                    textAlign: "center", //the song name is in the center of the bubble
+                    textAlign: "left", //the song name is in the center of the bubble
                   }}
                   key={item.name} //sets the key of the div to be the name of the item from the queue
                 >
@@ -156,7 +188,7 @@ function App() { // app function to wrap all the contents of the webpage
         }}><span style={{fontSize:'15px',fontWeight:'bolder'}}>Up Next:</span>  {peek()?.name}</p> {/* overwrites the styling of the "Up Next:" portion to be smaller and bolder, and then uses the queue peek function to render the song next up in the queue. the question mark between peek and name is needed in case there is nothing in the queue so it doesn't try to call name on a lack of object */}
       </div>
       <p className="read-the-docs"> {/* uses the "read-the-docs" styling on this text */}
-        Using React + Vite + MaterialUI {/* specifies what tools are being used to render this page */}
+        Dev Build [React + Vite + MaterialUI] {/* specifies what tools are being used to render this page */}
       </p>
     </>
   )
