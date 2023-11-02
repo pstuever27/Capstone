bd7bb31a59a8b58e9090cafc2847a6e51f044aef
MistrrrMedia <65423117+MistrrrMedia@users.noreply.github.com>
2023-11-02 10:48:24 -0500

    Fixed Input Focusing & Styling
    Backspace now focuses on the previous index. No default CSS styling for focused elements.

--- Splash Screen/src/index.js
+++ Splash Screen/src/index.js
@@ -5,8 +5,10 @@
  *              
  * Programmer's Name: Nicholas Nguyen
  * Date Created: 11/1/2023
- * Date Revised: 11/1/2023 - Nicholas Nguyen - Began Splashscreen UI Integration
- *  Revision: 11/1/2023 - Nicholas Nguyen - Began Splashscreen UI Integration
+ * 
+ * Date Revised: 11/1/2023 - Nicholas Nguyen
+ *  Revision: Began Splashscreen UI Integration
+ * 
  * Preconditions: 
  *  @inputs : None
  * Postconditions:
