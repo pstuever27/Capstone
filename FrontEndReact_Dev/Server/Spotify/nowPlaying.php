b4d271b499bccb105d5274dc94356d7bdf8fadaa
Paul Stuever <pstuever27@gmail.com>
2023-12-03 18:02:36 -0600

    commented php

--- FrontEndReact_Dev/Server/Spotify/nowPlaying.php
+++ FrontEndReact_Dev/Server/Spotify/nowPlaying.php
@@ -2,44 +2,55 @@
 require '../../vendor/autoload.php';
 require '../require/sql.php';
 
+//Get spotify app information from json (gitignore)
+$json = file_get_contents('../../../client.json');
+$appData = json_decode($json, true);
+
+//Create new session with our web app information
 $session = new SpotifyWebAPI\Session(
-  'CLIENT_ID', //CLIENT_ID
-  'CLIENT_SECRET', //CLIENT_SECRET 
+  $appData[0], //ClientID
+  $appData[1], //Client Secret
 );
 
+// Open sql connection
 $mysql = SQLConnect();
 
 $status = 'wait';
 
+//Prepare statement to get the access token and refresh token
 $stmt = $mysql->prepare("SELECT accessToken, refreshToken FROM room WHERE roomCode = ?");
 $stmt->bind_param('s', $_POST['roomCode']);
-$stmt->execute();
+$stmt->execute(); //Execute sql
 
+//If we have a token, then set that as our current 
 if ($accessToken) {
-
+  //Set the tokens as current in our session
   $session->setAccessToken($accessToken);
   $session->setRefreshToken($refreshToken);
 } else {
-
+  //Otherwise, just use the refresh token and it'll auto-refresh in the api call
   $session->refreshAccessToken($refreshToken);
 }
 
+//Set to auto refresh if needed
 $options = [
   'auto_refresh' => true,
 ];
 
+//Set up API
 $api = new SpotifyWebAPI\SpotifyWebAPI($options, $session);
 
 try {
+  //Get the currently playing track
   $response = $api->getMyCurrentTrack();
 
-} catch (SpotifyWebAPI\SpotifyWebAPIException $e) {
+} catch (SpotifyWebAPI\SpotifyWebAPIException $e) { //If there's an error, send error response
   $response = [
     'status' => 'error',
     'error' => $e->getMessage()
   ];
 }
-
+//Send response
 echo json_encode($response);
 
 ?>
