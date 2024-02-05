bafd2fe5f4455a6405715bb9b11eba8ec64637b2
RylanDeGarmo <112727608+RylanDeGarmo@users.noreply.github.com>
2024-02-05 13:22:42 -0600

    Update kick-guest.php

--- Server Testing Stuff/Server/kick-guest.php
+++ Server Testing Stuff/Server/kick-guest.php
@@ -7,8 +7,7 @@
  * Programmer's Name: Rylan DeGarmo
  * Date Created: 11/12/2023 - Rylan DeGarmo
  * Date Revised: 11/12/2023 - Rylan DeGarmo - Creation
- * Date Revised: 11/14/2023 - Rylan DeGarmo - Depreciated, Alt Implementation through guest-list used...
- * ...retained for potential future use
+ * Date Revised: 2/3/2024 - Rylan DeGarmo - Implementation of SQL added for use with react
  * Preconditions: 
  *  @inputs : N/A
  * Postconditions:
@@ -18,4 +17,39 @@
  * Invariants: None
  * Known Faults: N/A
  * **/
+
+ require_once('./require/sql.php');
+
+$mysql = SQLConnect();
+
+$status = 'wait';
+
+$stmt = $mysql->prepare('SELECT userName FROM client WHERE roomCode = ?');
+
+$stmt->bind_param('s', $_POST['roomCode']);
+$stmt->execute();
+
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
+
+if(!$result || !$row) {
+    $status = 'no-guests';
+
+    $response = [
+        'status' => $status
+    ];
+
+    echo json_encode($response);
+    return;
+}
+else{
+    $status = 'ok';
+        $myArray[] = $row;
+    while($row = $result->fetch_assoc()) {
+        $myArray[] = $row;
+    }
+    echo json_encode($myArray);
+}
+exit(200);
+
 ?>
