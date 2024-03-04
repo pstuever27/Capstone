9137faa1ead264a3da4717908ccec03c1b9f733e
RylanDeGarmo <rylansauras123@gmail.com>
2024-03-04 01:50:47 -0600

    Banned List commit (still working)

--- Server/Spotify/addToBanned.php
+++ Server/Spotify/addToBanned.php
@@ -17,4 +17,70 @@
 
 //Placeholder for after .jsx is finished
 
+require '../../vendor/autoload.php';
+require '../require/sql.php';
+
+header('Access-Control-Allow-Origin: *'); //Uncomment for local testing
+
+//Get spotify app information from json (gitignore)
+$info = file_get_contents('../../client.json');
+$json = json_decode($info);
+
+//Create new session with our web app information
+$session = new SpotifyWebAPI\Session(
+  $json->CLIENT_ID, //ClientID
+  $json->CLIENT_SECRET, //Client Secret
+);
+
+
+// Open sql connection
+$mysql = SQLConnect();
+
+$status = 'wait';
+
+//Prepare statement to get the access token and refresh token
+$stmt = $mysql->prepare("SELECT accessToken, refreshToken FROM room WHERE roomCode = ?");
+$stmt->bind_param('s', $_POST['roomCode']);
+$stmt->execute(); //Execute sql
+
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
+$accessToken = $row["accessToken"];
+$refreshToken = $row["refreshToken"];
+
+//If we have a token, then set that as our current 
+if ($accessToken) {
+  //Set the tokens as current in our session
+  $session->setAccessToken($accessToken);
+  $session->setRefreshToken($refreshToken);
+} else {
+ //Otherwise, just use the refresh token and it'll auto-refresh in the api call
+  $session->refreshAccessToken($refreshToken);
+}
+
+//Set to auto refresh if needed
+$options = [
+  'auto_refresh' => true,
+];
+
+//Set up API
+$api = new SpotifyWebAPI\SpotifyWebAPI($options, $session);
+
+//Try to add the song to the queue
+try {
+  $api->queue($_POST['query']);
+  //Send response
+  $response = [
+    'status' => 'ok',
+  ];
+} catch (SpotifyWebAPI\SpotifyWebAPIException $e) { //If there's an issue with the api call, then throw error
+  $response = [
+    'status' => 'error',
+    'error' => $e->getMessage()
+  ];
+}
+
+//Send response
+echo json_encode($response);
+
 ?>
--- src/pages/bannedList.jsx
+++ src/pages/bannedList.jsx
@@ -11,11 +11,123 @@
 // Faults: None
 //--------------------------------
 
+// useAPI, getAuthURl, and useHostAPI are necessary functions from SpotifyAPI.js
+import { useAPI, useHostAPI } from '../SpotifyAPI'; 
+
+// useState is used to perform side effects in function components. 
+import { useState } from 'react' 
+
+// allows usage of contexts
+import { useContext } from 'react';
+
+// utocomplete renders the search results of the search bar. 
+import { useAutocomplete } from '@mui/base/useAutocomplete';
+
+// imports queue context to manipulate queue data structure across components
+import QueueContext from './queueContext';
+
+// imports palette context to manipulate color palette across components
+import PaletteContext from './paletteContext';
+
+// imports authorizationApi for adding songs to the Banned queue
+import authorizationApi from '../authorizationApi';
+
+import { useLocation } from 'react-router-dom'
+
 function BanList() {
-    return (
-        <div>
+    // INITIALIZING STATE VARIABLES
+  
+    const { enqueue } = useContext( QueueContext );
+  
+    // This is the default inputVal (blank) for the search bar. 
+    const [inputVal, setInputValue] = useState( "" ); 
+  
+    // reqSearch is a function used to search for the value stored at inputVal
+    // The syntax `{ makeRequest: reqSearch }` uses JavaScript's destructuring assignment to 
+    //  extract the `makeRequest` function from the object returned by `useAPI`. 
+    //  It also renames `makeRequest` to `reqSearch` for use in this component.
+    const { makeRequest: reqSearch } = useAPI( 'https://api.spotify.com/v1/search' ); 
+  
+    const { makeRequest: reqPlayer } = useHostAPI( 'https://api.spotify.com/v1/me/player' ); 
+  
+    const { addToQueue: addQueue } = authorizationApi();
+  
+    // An empty list state is used to store search results.
+    // These search results will be rendered in the search bar dropdown. 
+    const [searchResults, setSearchRes] = useState( [] ); 
+  
+    // A null-initialized save state is used to have its value set as
+    //  the user-selected song from the search results. 
+    const [songChoice, setSongChoice] = useState( null ); 
+  
+    // An empty list state is used to store the blocked songs.
+    const [blockedSongs, setBlockedSong] = useState( [] ); 
+  
+    const { palette } = useContext(PaletteContext);
+    
+    const location = useLocation();
+  
+    const { getInputProps, getListboxProps, getOptionProps,  groupedOptions } = useAutocomplete( {
+      /***************************************************************************/
+      /* The following properties are used to configure the search bar dropdown. */
+      /***************************************************************************/
+      id: 'search-box',
+      value: songChoice,
+      inputValue: inputVal,
+      options: searchResults,
+      noOptionsText: 'Oops! Nothing :(',
+      autoHighlight: true,
+      disablePortal: true,
+  
+      onInputChange: (event, newInputValue) => {
+        setInputValue(newInputValue);
+        if (newInputValue !== "") {
+          search(); // Trigger search based on new input
+        }
+      },
+  
+      onChange: (event, newValue) => {
+        setSongChoice(newValue);
+      },
+  
+      isOptionEqualToValue: (option, value) => `${option.name} - ${option.artists[0].name}` === `${value.name} - ${value.artists[0].name}`,
+      getOptionLabel: (searchResult) => `${searchResult.name} - ${searchResult.artists.map((_artist) => _artist.name).join(", ")}`,
+    });
+
+    async function addToBanned() {
+        // Verifies that the user has selected a song from the search results & it isn't blocked
+        if( songChoice != null && !blockedSongs.includes( songChoice ) ){
+          // Adds songChoice to the visual queue on screen
+          
+          enqueue( songChoice ); 
+    
+          // If the user has logged into spotify and the callback result is in the url bar
+          if ( location.hash === '#/callback' || location.pathname === '/join') { 
+            // Parse the elements of the url, retain only the search query after `?` in the URL
+            let urlParams = new URLSearchParams( window.location.search ); 
+    
+            // Get the code from the url; if the url doesn't contain a code, set code variable to "empty"
+            let code = urlParams.get( 'code' ) || "empty";
+    
+            // calls the add to queue API request
+            // `.then( () => {} )` literally does nothing. `then()` is required syntax. 
+            addQueue( songChoice.id );    
+          }
+    
+          // if the url doesn't contain callback, it means they didn't login to spotify first 
+          else {
+            // throw an alert, error dialog window
+            alert( "Must login to manipulate the Spotify queue." ); 
+          }
+        }
+
+        // reinitializes the song choice to be empty
+        setSongChoice( null ); 
+    }
+
+    <div>
             
-            <button id="SongBan" className="banButton" onClick={banSong} disabled={isLoading}>
+            <button id="SongBan" className="banButton" onClick={() => addToBanned()} disabled={isLoading}>
                 Add to Ban List
             </button>
 
@@ -23,8 +135,7 @@ function BanList() {
                 Show Banned Songs
             </button>
 
-        </div>
-    );
+    </div>
 }
 
 export default BanList
