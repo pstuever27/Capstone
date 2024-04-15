3c9ee1aa42d6f7f86e3306c34e8021b4aaa0971c
Kieran <kdelaney472@gmail.com>
2024-04-15 16:53:10 -0500

    song removal added to queue

--- src/pages/queue.jsx
+++ src/pages/queue.jsx
@@ -7,6 +7,9 @@
 // Revised on: 02/09/2024
 // Revision: Nicholas changed the way song titles were being displayed in the queue
 //            also added color palette context to change the color of the dequeue button
+// Revised on: 04/15/2024
+// Revision: Kieran added an 'x' button to the end of each song rendered in the queue which removes that track from the queue when clicked
+//            also removed the dequeue button as we don't need it now, and made is so if they queue is empty it prints a little grey message letting the user know
 //
 // Preconditions: Must have npm and node installed to run in dev environment. 
 //                Also see SpotifyAPI.js for its preconditions.
@@ -30,7 +33,7 @@ import PaletteContext from './paletteContext';
 import authorizationApi from '../authorizationApi';
 
 function Queue() {
-  const { songQueue, dequeue } = useContext( QueueContext );
+  const { songQueue, setQueue, dequeue } = useContext( QueueContext );
   const { palette } = useContext( PaletteContext );
 
   return (
@@ -39,18 +42,26 @@ function Queue() {
           {/* <h1>Queue</h1> */}
           <div id = "queueGrid">       
             {/* maps through the songQueue array and renders each song in the queue as a grid row */}
-            { songQueue?.map( ( song ) => { 
-              // return a grid column for the queue render; renders artist name and title
-              return( <div id = "queueGridCol" span={ 12 } key={ song.name } >
-                <p id = "qname">
-                  { song.name }
-                  <span id = "qremove" onClick={() => { alert("bean"); }} >&times;</span>
-                </p>
-                <p id = "qartists">{ song.artists.map((_artist) => _artist.name).join(", ") }</p>
-              </div> );
-            } ) } 
+            { (songQueue?.length == 0)
+              ? //true
+              <p style={{color:"lightgrey"}}>Queue is empty.</p>
+              : //false
+              songQueue?.map( ( song ) => { 
+                // return a grid column for the queue render; renders artist name and title
+                return( <div id = "queueGridCol" span={ 12 } key={ song.name } >
+                  <p id = "qname">
+                    { song.name }
+                    <span id = "qremove" onClick={() => { 
+                      const modifiedQueue = songQueue.filter(songs => songs !== song); //removes current track with 'x' being clicked from queue
+                      setQueue(modifiedQueue); //updates the queue to no longer have that removed track
+                    }} >&times;</span> {/* "&times;" renders the X symbol on the screen as a remove symbol to be clicked */}
+                  </p>
+                  <p id = "qartists">{ song.artists.map((_artist) => _artist.name).join(", ") }</p>
+                </div> );
+              } ) } 
           </div>
-          <button id = "dequeue" onClick = { dequeue } style={{ backgroundColor: palette[1]}}>Dequeue</button>
+          {/* <button id = "dequeue" onClick = { dequeue } style={{ backgroundColor: palette[1]}}>Dequeue</button> */}
+            {   /*  ^ no longer need the dequeue button since we can remove specific tracks using the X buttons */}
         </div>
     </>
   );
